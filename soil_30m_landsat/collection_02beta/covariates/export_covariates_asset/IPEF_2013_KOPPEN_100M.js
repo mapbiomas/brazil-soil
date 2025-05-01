8dcc6234357df1f0f4ec2fb1927e1083bb617661
samuel-rosa <alessandrosamuelrosa@gmail.com>
2025-05-01 19:59:49 -0300

    Add IPEF_2013_KOPPEN_100M script for Koppen climate classification and dummy variable generation

--- soil_30m_landsat/collection_02beta/covariates/export_covariates_asset/IPEF_2013_KOPPEN_100M.js
+++ soil_30m_landsat/collection_02beta/covariates/export_covariates_asset/IPEF_2013_KOPPEN_100M.js
@@ -1,27 +1,34 @@
-/* 
-PROJETO MAPBIOMAS - Solo | GT Solos - Pacote de Trabalho: Mapeamento Espaço-Temporal de Propriedades do Solo
-Dataset Dummies-koppen * Asset com a classificação climatica de koppen
+/*
+ * Dataset: Koppen Climate Classification
+ * 
+ * Authors:
+ * - Wallace Silva
+ * - Barbara Silva
+ * - Taciara Horst
+ * - Marcos Cardoso
+ * - David Pontes
+ * 
+ * Changes:
+ * - 2022-06-01 (Wallace, Barbara): Generating dummy variables for Koppen climate classification
+ * - 2024-08-12 (Marcos, David) Filling empty pixels with interpolation
+ * - 2025-25-04 (Taciara): Renaming classes
+ * 
+ * Contact: contato@mapbiomas.org
+ * 
+ * MapBiomas Soil
+ */
 
-Data: 2023-10-10
-Autores: Wallace Silva, Taciara Horst, Marcos Cardoso e David Pontes
-
-Contato: contato@mapbiomas.org
-Referencia https://docs.google.com/spreadsheets/d/1RlkNoJYCipVo7eJ9Fo56YjleiBmae2NYTFaeie4Q4XM/edit#gid=0
-*/
-
-// Carrega uma imagem que contém informações climáticas na projeção de Köppen.
 var koppen = ee.Image('projects/mapbiomas-solos-workspace/assets/covariates/climate/IPEF_koppen_30m');
 
-// Define uma paleta de cores para exibir as classes da imagem de Köppen.
 var palette = [
   "8FEE94","0A5DE1","183B95","C8EC4D","68C862","CDCD31","FAEF3C","73EB31","F8A62F","93E6FD","376E2B","0C9BFB"
 ];
 
-// Adiciona a imagem de Köppen ao mapa com a paleta de cores definida e define os valores mínimo e máximo para visualização.
+
 Map.addLayer(koppen,{palette:palette,min:1,max:12},'koppen',true);
 
-// Centraliza o mapa na imagem de Köppen.
 Map.centerObject(koppen);
+
 
 // interface
 var panel = ui.Panel({
@@ -33,11 +40,7 @@ var panel = ui.Panel({
   }
 });
 
-// Adiciona o painel de interface ao mapa.
 Map.add(panel);
-
-// Define um dicionário que mapeia os códigos de classe de Köppen para valores e cores correspondentes.
-  // É criado uma lista com as diferentes classes do sistema de classificação de Köppen, cada uma com seu valor e cor associados.
 
 var dictionary = {
  "Cwa":{
@@ -102,125 +105,143 @@ var dictionary = {
  }
 };
 
-// Define três níveis de agrupamento das classes de Köppen com base em características climáticas.
-  // Cada objeto neste array representa um nível de agrupamento.
 var levels = [
     {
-      name:'lv1',
+      //name:'lv1',
+      name:'koppen_l1',
       classes:[
         {
-          name:'Tropical',
+          //name:'Tropical',
+          name:'A',
           classe:[3,2,10,12],
         },
         {
-          name:'Dry_season',
+          //name:'Dry_season',
+          name:'B',
           classe:[9],
         },
         {
-          name:'Humid_subtropical_zone',
+          //name:'Humid_subtropical_zone',
+          name:'C',
           classe:[4,8,7,6,1,5,11]
         },
       ]
     },
     {
-      name:'lv2',
+      //name:'lv2',
+      name:'koppen_l2',
       classes:[
         {
-          name:'without_dry_season',
+          //name:'without_dry_season',
+          name:'Af',
           classe:[3],
         },
         {
-          name:'monsoon',
+          //name:'monsoon',
+          name:'Am',
           classe:[2],
         },
         {
-          name:'with_dry_summer',
+          //name:'with_dry_summer',
+          name:'As',
           classe:[10],
         },
         {
-          name:'tropical_with_dry_winter',
-          classe:[12],
+          //name:'with_dry_winter',
+          name:'Aw',
+          classe:[12], 
         },
         {
-          name:'semiarid',
+          //name:'semiarid',
+          name:'Bs',
           classe:[9],
         },
         {
-          name:'oceanic_climate_without_sry_season',
-          classe:[4,8,7,6],
+          //name:'oceanic_climate_without_sry_season',
+          name:'koppen_Cf',
+          classe:[4,8],
+        },
+                {
+          //name:'with_dry_summer',
+          name:'Cs',
+          classe:[7,6],
         },
         {
-          name:'temperate_with_dry_winter',
+          //name:'with_dry_winter',
+          name:'Cw',
           classe:[1,5,11],
         },
       ]
     },
     {
-      name:'lv3',
+      //name:'lv3',
+      name:'koppen_l3',
       classes:[
         {
-          name:'low_latitude_and_altitude',
+          //name:'low_latitude_and_altitude',
+          name:'Bsh',
           classe:[9],
         },
         {
-          name:'with_hot_summer',
+          //name:'with_hot_summer',
+          name:'Cfa',
           classe:[4],
         },
         {
-          name:'with_temperate_summer',
+          //name:'with_temperate_summer',
+          name:'Cfb',
           classe:[8],
         },
         {
-          name:'and_hot',
+          //name:'and_hot',
+          name:'Csa',
           classe:[7],
         },
         {
-          name:'and_temperate',
+          //name:'and_temperate',
+          name:'Csb',
           classe:[6],
         },
         {
-          name:'and_hot_summer',
+          //name:'and_hot_summer',
+          name:'Cwa',
           classe:[1],
         },
         {
-          name:'and_temperate_summer',
+          //name:'and_temperate_summer',
+          name:'Cwb',
           classe:[5],
         },
         {
-          name:'and_short_and_cool_summer',
+          //name:'and_short_and_cool_summer',
+          name:'Cwc',
           classe:[11],
         },
       ]
     },
   ];
- 
-// Itera sobre os níveis de agrupamento.
-  // Cria uma imagem vazia para armazenar as classes de Köppen para o nível atual.
-levels.forEach(function(object){
 
-// Cria uma imagem vazia para armazenar as classes de Köppen para o nível atual.
+levels.forEach(function(object){
+  
   var recipe = ee.Image().select();
-    // Gera uma máscara para a classe de Köppen atual.
+  
   object.classes.forEach(function(obj){
     // print( object.name + '  ' + obj.name + ' ' + obj.classe);
-  
-    // Gera uma máscara para a classe de Köppen atual.
+    
     var dummy = koppen
       .eq(obj.classe)
       .reduce('max')
       .gte(1)
       .rename(object.name + '_' + obj.name);
- 
-   // Adiciona a máscara à imagem vazia.
+      
     recipe = recipe.addBands(dummy);
 
   });
 
-// Define o local de armazenamento e outras informações para a imagem resultante.
-  var output = 'projects/mapbiomas-workspace/SOLOS/COVARIAVEIS/IPEF_KOPPEN_30M_DUMMY_COLECAO1/';
+  var output = 'projects/mapbiomas-workspace/SOLOS/COVARIAVEIS/IPEF_2013_KOPPEN_100M/';
   var description = object.name;
 
-// Define metadados para a imagem resultante.
+  
   recipe = recipe.set({
     'index':description,
     'version':'v1',
@@ -229,12 +250,10 @@ levels.forEach(function(object){
     });
   
   print(object.name,recipe);
-
-// Adiciona a imagem resultante ao mapa.
+  
   Map.addLayer(recipe,{},object.name);
   Map.addLayer(koppen.geometry(),{},'bounds');
-
-// Exporta a imagem resultante como um ativo no Google Earth Engine.
+  
   Export.image.toAsset({
     image:recipe,
     description:description,
